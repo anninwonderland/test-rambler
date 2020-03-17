2e00dc8a1f1726757c12295dec5d530e463bc872
@anninwonderland <asrakina@miem.hse.ru>
2020-03-18 00:04:52 +0300

    add basic structure

---
+++ test-rakina/src/components/Feed/Feed.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div>
+    <p>
+        feed
+    </p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Feed"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ test-rakina/src/views/Feed.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Feed/>
+  </div>
+</template>
+
+<script>
+import Feed from "../components/Feed/Feed";
+
+export default {
+  name: 'Home',
+  components: {
+    Feed
+  }
+}
+</script>
