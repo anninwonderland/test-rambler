5f1bef7635f28bbde8ad60bdcdf5cd55bb4ce84d
@anninwonderland <asrakina@miem.hse.ru>
2020-03-20 17:18:44 +0300

    fix autolayout for post component🥵

--- src/components/Feed/Feed.vue
+++ src/components/Feed/Feed.vue
@@ -1,35 +1,18 @@
 <template>
     <el-row>
-        <el-col :offset="2" :span="20" class="hidden-md-and-up">
-            <PostSm/>
-        </el-col>
-        <el-col :offset="2" :span="20" class="hidden-md-and-up">
-            <PostSm/>
-        </el-col>
-        <el-col :offset="2" :span="20" class="hidden-md-and-up">
-            <PostSm/>
-        </el-col>
-
-        <el-col :offset="2" :span="6" class="hidden-sm-and-down">
-            <PostLg/>
-        </el-col>
-        <el-col :offset="1" :span="6" class="hidden-sm-and-down">
-            <PostLg/>
-        </el-col>
-        <el-col :offset="1" :span="6" class="hidden-sm-and-down">
-            <PostLg/>
+        <el-col :offset="2" :span="20" class="">
+            <Post/>
         </el-col>
     </el-row>
 </template>
 
 <script>
-    import PostSm from "./Post/PostSm";
-    import PostLg from "./Post/PostLg";
+    import Post from "./Post/Post";
     import axios from 'axios';
 
     export default {
         name: 'Feed',
-        components: { PostSm, PostLg },
+        components: { Post },
         data() {
             return {
                 data: [],
--- src/components/Feed/Post/Post.vue
+++ src/components/Feed/Post/Post.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="post-wrapper post-wrapper-sm">
-        <img class="post-img-sm"
+    <div class="post-wrapper">
+        <img class="post-img-sm hidden-sm-and-up"
              src="https://avatars.mds.yandex.net/get-pdb/985791/25a243bc-9b31-4e8d-a18e-8c16a2ff008b/s1200"
              alt="Posted photo">
 
@@ -15,8 +15,9 @@
                 </div>
                 <span class="date">3h</span>
             </div>
-            <hr class="divider hidden-md-and-up">
+            <hr class="divider hidden-sm-and-up">
             <div class="content">
+                <img class="post-img hidden-xs-only" src="https://avatars.mds.yandex.net/get-pdb/985791/25a243bc-9b31-4e8d-a18e-8c16a2ff008b/s1200" alt="Posted photo">
                 <div class="like">
                     <div class="like-btn"></div>
                     <span>12 000</span>
@@ -31,7 +32,7 @@
 
 <script>
     export default {
-        name: 'PostSm'
+        name: 'Post'
     }
 </script>
 
