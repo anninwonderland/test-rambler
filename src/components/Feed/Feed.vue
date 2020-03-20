954428db26384dade6b375151f34811dc093d595
@anninwonderland <asrakina@miem.hse.ru>
2020-03-20 16:45:45 +0300

    add requests for fetching data

--- src/components/Feed/Feed.vue
+++ src/components/Feed/Feed.vue
@@ -19,10 +19,6 @@
         <el-col :offset="1" :span="6" class="hidden-sm-and-down">
             <PostLg/>
         </el-col>
-        <!--        <el-col :offset="1" :span="6">-->
-        <!--            <PostSm/>-->
-        <!--        </el-col>-->
-
     </el-row>
 </template>
 
@@ -34,6 +30,13 @@
     export default {
         name: 'Feed',
         components: { PostSm, PostLg },
+        data() {
+            return {
+                data: [],
+                currentPage: 1,
+                pages: null,
+            }
+        },
         created() {
             this.fetchData()
         },
@@ -41,36 +44,44 @@
             fetchData() {
                 const apiKey = {
                     key: '592415d6611422a407ddf84227d38375',
-                    userId: 'cedarsphoto'
                 };
-                axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey.key}&user_id=${apiKey.userId}&format=json&nojsoncallback=1`)
+
+                axios.get(`https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${apiKey.key}&content_type=1&format=json&nojsoncallback=1&page=${this.currentPage}`)
                     .then(response => {
-                       //do some crash defend
-                        console.log(response.data)
-                        if (!response.data.photos.photo.length) return;
-                        let array = response.data.photos.photo;
-                        array.forEach(item => {
-                            axios.get(`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`)
-                                .then((response) => {
-                                    console.log(response.config.url);
-                                });
+                        const photosArr = response.data.photos.photo;
+                        this.pages = response.data.photos.pages;
+
+                        photosArr.forEach(item => {
+                            const post = {};
+
                             axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getFavorites&api_key=${apiKey.key}&photo_id=${item.id}&format=json&nojsoncallback=1`)
                                 .then((response) => {
-                                console.log(response.data.photo.total);
-                            })
+                                    post.likes = response.data.photo.total;
+                                });
+
                             axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${apiKey.key}&photo_id=${item.id}&format=json&nojsoncallback=1`)
                                 .then((response) => {
-                                    console.log(response.data.photo.dateuploaded, new Date(Number(response.data.photo.dateuploaded)*1000).toLocaleDateString("en-US"));
-                                    console.log(response.data.photo.owner.username);
-                                    console.log(response.data.photo.title._content);
-                                    console.log(response.data.photo.description._content);
-                                    console.log(response.data.photo.owner.location);
+                                    post.username = response.data.photo.owner.username;
+                                    post.location = response.data.photo.owner.location;
+                                    post.date = new Date(Number(response.data.photo.dateuploaded)*1000).toLocaleDateString("en-US");
+                                    post.title = response.data.photo.title._content;
+                                    post.description = response.data.photo.description._content;
+
+                                    post.avatar = (response.data.photo.owner.iconserver > 0) ?
+                                        `http://farm${response.data.photo.owner.iconfarm}.staticflickr.com/${response.data.photo.owner.iconserver}/buddyicons/${response.data.photo.owner.nsid}.jpg`
+                                        : 'https://www.flickr.com/images/buddyicon.gif'
                                 });
-                        })
+
+                            post.image = `https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`;
+
+                            this.data.push(post);
+                        });
+                        console.log(this.data)
                     })
                     .catch(err => {
                         console.log(err);
                     });
+
             }
         }
     }
