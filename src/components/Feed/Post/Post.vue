b886665cf69fe8a125bfd8db7bbfcd5e174929af
@anninwonderland <asrakina@miem.hse.ru>
2020-03-20 17:38:44 +0300

    add props to post component

--- src/components/Feed/Post/Post.vue
+++ src/components/Feed/Post/Post.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="post-wrapper" v-if="item">
         <img class="post-img-sm hidden-sm-and-up"
-             src="https://avatars.mds.yandex.net/get-pdb/985791/25a243bc-9b31-4e8d-a18e-8c16a2ff008b/s1200"
+             :src="item.image"
              alt="Posted photo">
 
         <div class="post-text">
@@ -24,7 +24,7 @@
                     <div class="like-btn"></div>
                     <span>{{item.likes}}</span>
                 </div>
-                <p class="post-desc" v-if="item.title">
+                <p class="post-title" v-if="item.title">
                     {{item.title}}
                 </p>
                 <p class="post-desc" v-if="item.description">
